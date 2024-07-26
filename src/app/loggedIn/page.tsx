a2fd5a73e4d75d7e6bd8926af0fd06f09ac13f81
lazycoder1 <gautamgsabhahit@gmail.com>
2024-07-26 20:25:07 +0530

    adding loading and switching to jiffyscan paymaster

--- src/app/loggedIn/page.tsx
+++ src/app/loggedIn/page.tsx
@@ -4,47 +4,44 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAccount, useDisconnect, useWalletClient } from "wagmi";
 import { usePublicClient } from "wagmi";
 import { useRouter } from "next/navigation";
-import {
-    SmartAccount,
-    signerToSimpleSmartAccount
-} from "permissionless/accounts";
+import { SmartAccount, signerToSimpleSmartAccount } from "permissionless/accounts";
 import { Address, Chain, Hash, Transport, http } from "viem";
-import {
-    ENTRYPOINT_ADDRESS_V06,
-    SmartAccountClient,
-    createSmartAccountClient,
-    walletClientToSmartAccountSigner,
-} from "permissionless";
+import { ENTRYPOINT_ADDRESS_V06, SmartAccountClient, createSmartAccountClient, walletClientToSmartAccountSigner } from "permissionless";
 import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
 import { sepolia, fuse } from "viem/chains";
 import { ENTRYPOINT_ADDRESS_V06_TYPE } from "permissionless/types";
 import DemoTransactionButton from "../components/demo-transaction";
 
-import { usePrivy, useWallets } from '@privy-io/react-auth';
-import { useSetActiveWallet } from '@privy-io/wagmi';
+import { usePrivy, useWallets } from "@privy-io/react-auth";
+import { useSetActiveWallet } from "@privy-io/wagmi";
 import Mint from "./Mint";
 import { JiffyPaymaster } from "@jiffy-labs/web3a";
 
 const jiffyscanKey = process.env.NEXT_PUBLIC_JIFFYSCAN_API_KEY as string;
 
-if (!process.env.NEXT_PUBLIC_PRIVY_APP_ID)
-    throw new Error("Missing NEXT_PUBLIC_PRIVY_APP_ID");
+if (!process.env.NEXT_PUBLIC_PRIVY_APP_ID) throw new Error("Missing NEXT_PUBLIC_PRIVY_APP_ID");
 
-if (!process.env.NEXT_PUBLIC_PIMLICO_API_KEY)
-    throw new Error("Missing NEXT_PUBLIC_PIMLICO_API_KEY");
+if (!process.env.NEXT_PUBLIC_PIMLICO_API_KEY) throw new Error("Missing NEXT_PUBLIC_PIMLICO_API_KEY");
 
+if (!process.env.NEXT_PUBLIC_JIFFYSCAN_API_KEY) throw new Error("Missing NEXT_PUBLIC_JIFFYSCAN_API_KEY");
 
-if (!process.env.NEXT_PUBLIC_JIFFYSCAN_API_KEY)
-    throw new Error("Missing NEXT_PUBLIC_JIFFYSCAN_API_KEY");
+if (!process.env.NEXT_PUBLIC_BUNDLER_URL) throw new Error("Missing NEXT_PUBLIC_BUNDLER_URL");
 
 // Define the chains with their respective entry points and RPC URLs
 const chains = [
-    { name: "Fuse", chain: fuse, rpcUrl: `https://api.pimlico.io/v2/${fuse.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`, explorerUrl: "https://explorer.fuse.io/tx/" },
-    { name: "Sepolia", chain: sepolia, rpcUrl: `https://api.pimlico.io/v2/${sepolia.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`, explorerUrl: "https://sepolia.etherscan.io/tx/" }
+    {
+        name: "Fuse",
+        chain: fuse,
+        rpcUrl: `https://api.pimlico.io/v2/${fuse.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`,
+        explorerUrl: "https://explorer.fuse.io/tx/",
+    },
+    {
+        name: "Sepolia",
+        chain: sepolia,
+        rpcUrl: `https://api.pimlico.io/v2/${sepolia.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`,
+        explorerUrl: "https://sepolia.etherscan.io/tx/",
+    },
 ];
-
-
-
 
 function LoggedIn() {
     const router = useRouter();
@@ -54,15 +51,9 @@ function LoggedIn() {
     const publicClient = usePublicClient();
     const { data: walletClient } = useWalletClient();
     const [txHash, setTxHash] = useState<string | null>(null);
-    const [smartAccountClient, setSmartAccountClient] =
-        useState<SmartAccountClient<ENTRYPOINT_ADDRESS_V06_TYPE> | null>(
-            null
-        );
+    const [smartAccountClient, setSmartAccountClient] = useState<SmartAccountClient<ENTRYPOINT_ADDRESS_V06_TYPE> | null>(null);
     const [selectedChain, setSelectedChain] = useState(chains[0]);
-    const embeddedWallet = useMemo(
-        () => wallets.find((wallet) => wallet.walletClientType === "privy"),
-        [wallets]
-    );
+    const embeddedWallet = useMemo(() => wallets.find((wallet) => wallet.walletClientType === "privy"), [wallets]);
     const { user, ready, authenticated, logout } = usePrivy();
     const { setActiveWallet } = useSetActiveWallet();
 
@@ -70,12 +61,15 @@ function LoggedIn() {
     //     transport: http(selectedChain.rpcUrl),
     //     entryPoint: ENTRYPOINT_ADDRESS_V06
     // })
-    
+    const bundlerUrl = process.env.NEXT_PUBLIC_BUNDLER_URL as string;
+    const jiffyPaymaster = new JiffyPaymaster(bundlerUrl, selectedChain.chain.id, {
+        "x-api-key": jiffyscanKey,
+    });
+
     const bundlerClient = createPimlicoBundlerClient({
         transport: http(selectedChain.rpcUrl),
         entryPoint: ENTRYPOINT_ADDRESS_V06,
-    })
-    
+    });
 
     useEffect(() => {
         if (embeddedWallet) {
@@ -94,17 +88,14 @@ function LoggedIn() {
     }, [disconnect]);
 
     useEffect(() => {
-        ; (async () => {
+        (async () => {
             if (isConnected && walletClient && publicClient) {
                 const customSigner = walletClientToSmartAccountSigner(walletClient);
 
-                const simpleSmartAccountClient = await signerToSimpleSmartAccount(
-                    publicClient,
-                    {
-                        entryPoint: ENTRYPOINT_ADDRESS_V06,
-                        signer: customSigner
-                    }
-                );
+                const simpleSmartAccountClient = await signerToSimpleSmartAccount(publicClient, {
+                    entryPoint: ENTRYPOINT_ADDRESS_V06,
+                    signer: customSigner,
+                });
 
                 const smartAccountClient = createSmartAccountClient({
                     account: simpleSmartAccountClient,
@@ -112,11 +103,12 @@ function LoggedIn() {
                     chain: selectedChain.chain,
                     bundlerTransport: http(process.env.NEXT_PUBLIC_BUNDLER_URL, {
                         fetchOptions: {
-                          headers: { "x-api-key": jiffyscanKey },
+                            headers: { "x-api-key": jiffyscanKey },
                         },
-                      }),
+                    }),
                     middleware: {
                         gasPrice: async () => (await bundlerClient.getUserOperationGasPrice()).fast,
+                        sponsorUserOperation: jiffyPaymaster.sponsorUserOperationV6,
                     },
                 });
 
@@ -131,7 +123,7 @@ function LoggedIn() {
 
     const handleChainChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedChainId = parseInt(e.target.value);
-        const selected = chains.find(chain => chain.chain.id === selectedChainId);
+        const selected = chains.find((chain) => chain.chain.id === selectedChainId);
         if (selected) {
             setSelectedChain(selected);
         }
@@ -165,10 +157,7 @@ function LoggedIn() {
                         </div>
                         <div className="mt-4 flex flex-col space-y-4">
                             <Mint smartAccountClient={smartAccountClient} onSendTransaction={onSendTransaction} />
-                            <DemoTransactionButton
-                                smartAccountClient={smartAccountClient}
-                                onSendTransaction={onSendTransaction}
-                            />
+                            <DemoTransactionButton smartAccountClient={smartAccountClient} onSendTransaction={onSendTransaction} />
                             <button
                                 onClick={logout}
                                 className="mt-6 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
